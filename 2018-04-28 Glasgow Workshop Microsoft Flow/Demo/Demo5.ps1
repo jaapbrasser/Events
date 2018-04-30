acbd3d319a628dad9bf32a455fb3bdd3f565269a
Jaap Brasser <noreply@jaapbrasser.com>
2018-04-30 11:05:41 +0200

    Added Glasgow Flow workshop

---
+++ 2018-04-28 Glasgow Workshop Microsoft Flow/Demo/Demo5.ps1
@@ -0,0 +1,19 @@
+$FlowModulePath = 'C:\users\Jaap Brasser\OneDrive\Documents\Events\2018-04 WinOps\Demo\Helper\PowerShellFlow.psm1'
+Import-Module $FlowModulePath
+
+Get-Command -Module PowerShellFlow
+
+# Check Data Gateway service
+Get-Service PBIEgwService
+
+# Switch to Admin PowerShell console
+$FlowModulePath = 'C:\users\Jaap Brasser\OneDrive\Documents\Events\2018-04 WinOps\Demo\Helper\PowerShellFlow.psm1'
+Import-Module $FlowModulePath
+Start-FlowAgent
+
+# Switch to Phone
+
+# How to make this more resilient
+if (-not (Get-FlowAgentStatus)) {
+    Start-FlowAgent
+}
