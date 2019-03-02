9df579788790af595214bb4e86d3b658d88ef9a0
Jaap Brasser <noreply@jaapbrasser.com>
2019-03-02 01:41:19 +0100

    Added demo functions and csv demo

---
+++ 2019-02-28 Netwrix/Demo/Demo Functions.ps1
@@ -0,0 +1,30 @@
+function Get-ExternalMailRules {
+    param (
+        [pscredential] $Credential,
+        [string[]] $TrustedDomain
+    )
+
+    $Splat = @{
+        ConfigurationName = 'Microsoft.Exchange'
+        ConnectionUri = 'https://outlook.office365.com/powershell-liveid/'
+        Credential = 'jaap@everythingisawesome.onmicrosoft.com'
+        Authentication = 'Basic'
+        AllowRedirection = $true
+    }
+
+    $Session = New-PSSession @Splat
+
+    Invoke-Command -Session $Session {Get-MailBox | Get-InboxRule -ErrorAction SilentlyContinue} |
+    Where-Object {
+        # Regex extracts only the domainname from the ForwardTo / Forward
+        if ($_.ForwardTo) {
+            if (($_.ForwardTo -replace '.*?".*?@(.*?)".*','$1') -notin $TrustedDomain) {
+                $true
+            }
+        } elseif ($_.ForwardAsAttachmentTo) {
+            if (($_.ForwardAsAttachmentTo -replace '.*?".*?@(.*?)".*','$1') -notin $TrustedDomain) {
+                $true
+            }
+        }
+    } | Select-Object -Property MailBoxOwnerId, Name, ForwardTo, ForwardAsAttachmentTo
+}
